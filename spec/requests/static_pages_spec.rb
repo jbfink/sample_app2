ad46452db83ba7438b036aaff0c33733b7d87276
jbfink <john.fink@gmail.com>
2012-06-21 19:31:42 -0400

    All green.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -26,11 +26,11 @@ end
 describe "About page" do
      it "should have the h1 'About Us'" do
 	visit '/static_pages/about'
-	page.should have_content('h1', :text => 'About Us')
+	page.should have_selector('h1', :text => 'About Us')
 end
      it "should have the title 'About Us'" do
 	visit '/static_pages/about'
-	page.should have_content('title', :text => "Ruby on Rails Tutorial Sample App | About Us")
+	page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | About Us")
 end
 end
 end
