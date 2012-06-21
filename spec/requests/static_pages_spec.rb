51f59150e79bb1c27f81fb1a8bbf18524de0b3b4
jbfink <john.fink@gmail.com>
2012-06-21 14:31:58 -0400

    Speling error in test.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -17,7 +17,7 @@ describe "Help page" do
 	visit '/static_pages/help'
 	page.should have_selector('h1', :text => 'Help')
 end
-     it "should have the titel 'Help'" do
+     it "should have the title 'Help'" do
         visit '/static_pages/help'
         page.should have_selector('title',
 			:text => "Ruby on Rails Tutorial Sample App | Help")
