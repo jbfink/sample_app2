6824c726fb5e28637bd9497aefd7c972e415ee11
jbfink <john.fink@gmail.com>
2012-06-21 14:56:48 -0400

    I am an idiot.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe "StaticPages" do
   describe "Home page" do
      it "should have the title 'Home'" do
-     visit 'static_pages/home'
+     visit '/static_pages/home'
      page.should have_selector('title',
 			:text => "| Home")
 end
