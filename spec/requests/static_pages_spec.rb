36ed52a484b07ab3b9b758211bdcba55335d3c49
jbfink <john.fink@gmail.com>
2012-06-22 14:56:42 -0400

    Finished 3.5 exercise one.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -34,13 +34,13 @@ end
 end
 end
 describe "Contact page" do
-     it "should have the h1 'Contact Us'" do
+     it "should have the h1 'Contact'" do
 	visit '/static_pages/contact'
-	page.should have_selector('h1', :text => 'Contact Us')
+	page.should have_selector('h1', :text => 'Contact')
 end
-     it "should have the title 'Contact Us'" do
+     it "should have the title 'Contact'" do
 	visit '/static_pages/contact'
-	page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Contact Us")
+	page.should have_selector('title', :text => "Ruby on Rails Tutorial Sample App | Contact")
 end
 end
 end
