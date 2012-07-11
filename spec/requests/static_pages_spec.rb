6dbe228295f580aefda21fb7b9b0c789118b9004
jbfink <john.fink@gmail.com>
2012-07-11 13:43:34 -0400

    Listing 5.36, testing links on layout.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -35,6 +35,21 @@ describe "Contact page" do
      let(:page_title)	{ 'Contact' }
      it_should_behave_like "all static pages"
 end
+it "should have the right links in the layout" do
+    visit root_path
+    click_link "About"
+    page.should have_selector 'title', text: full_title('About Us')
+    click_link "Help"
+    page.should have_selector 'title', text: full_title('Help')
+    click_link "Contact"
+    page.should have_selector 'title', text: full_title('Contact')
+    click_link "Home"
+    click_link "Sign up now!"
+    page.should have_selector 'title', text: full_title('Sign up')
+    click_link "sample app"
+    page.should have_selector 'title', text: full_title('')
+end
+
 end
 
 
