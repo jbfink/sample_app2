f7493d7cea37b034c91db63a68e1829c16850931
jbfink <john.fink@gmail.com>
2012-07-12 09:44:02 -0400

    Listing 6.14, test for too long name.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -29,5 +29,9 @@ end
     before { @user.email = " " }
     it { should_not be_valid }
 end
+  describe "when name is too long" do
+   before { @user.name = "a" * 51 }
+   it { should_not be_valid }
+end
 
 end
