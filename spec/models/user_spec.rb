0e05cdd307fd4c8823ef982e869fbdf9c3464956
jbfink <john.fink@gmail.com>
2012-07-12 12:14:37 -0400

    Listing 6.18, email address duplication test.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,7 +12,9 @@
 require 'spec_helper'
 
 describe User do
-  before { @user = User.new(name: "Example User", email: "user@example.com") }
+  before do 
+ @user = User.new(name: "Example User", email: "user@example.com") 
+end
 
   subject { @user }
 
@@ -52,4 +54,11 @@ end
      end
     end
    end
+  describe "when email address is already taken" do
+    before do
+     user_with_same_email = @user.dup
+     user_with_same_email.save
+     end
+     it { should_not be_valid } 
+ end
 end
