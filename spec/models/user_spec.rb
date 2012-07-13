7641f5ae142b69c2729e1ae77a2d2024335d033f
jbfink <john.fink@gmail.com>
2012-07-13 11:49:45 -0400

    Listing 6.31, test for email downcase.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -77,4 +77,12 @@ end
   before { @user.password_confirmation = nil }
     it { should_not be_valid }
 end
+  describe "email address with mixed case" do
+   let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+    it "should be saved as all lower case" do
+     @user.email = mixed_case_email
+     @user.save
+     @user.reload.email.should == mixed_case_email.downcase
+   end
+  end
 end
