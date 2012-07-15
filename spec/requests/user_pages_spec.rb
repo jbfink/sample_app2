ddc2345b6159c30c6b8e31c496c9fc63614c308c
jbfink <john.fink@gmail.com>
2012-07-14 21:42:33 -0400

    Listing 7.6.

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,10 +1,16 @@
 require 'spec_helper'
 
-describe "UserPages" do
+describe "User Pages" do
    subject { page }
    describe "Signup page" do
      before { visit signup_path }
      it { should have_selector('h1',	text: 'Sign up') }
      it { should have_selector('title',	text: full_title('Sign up')) }
   end
+describe "profile page" do
+ # code to make a user variable
+ before { visit user_path(user) }
+ it { should have_selector('h1',	text: user.name) }
+ it { should have_selector('title',	text: user.name) }
+end
 end
