19d84e104871015399e7d11fe5f7135134f923ff
jbfink <john.fink@gmail.com>
2012-07-14 21:50:16 -0400

    Listing 7.9, FactoryGirl fill in in spec/requests/user_pages_spec.rb

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -8,7 +8,7 @@ describe "User Pages" do
      it { should have_selector('title',	text: full_title('Sign up')) }
   end
 describe "profile page" do
- # code to make a user variable
+ let(:user) { FactoryGirl.create(:user) }
  before { visit user_path(user) }
  it { should have_selector('h1',	text: user.name) }
  it { should have_selector('title',	text: user.name) }
