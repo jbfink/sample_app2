7981c9e5789834e1fe599cef54282fd62a74fec7
jbfink <john.fink@gmail.com>
2012-08-08 13:03:02 -0400

    7.6 Exercise 2, Listing 7.31. Bunch of tests for errors.

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -20,6 +20,16 @@ describe "signup" do
   it "should not create a user" do
    expect { click_button submit }.not_to change(User, :count)
   end
+ describe "after submission" do
+  before { click_button submit }
+  it { should have_selector('title', text: 'Sign up') }
+  it { should have_content('error') }
+  it { should have_content("Password can't be blank") }
+  it { should have_content("Name can't be blank") }
+  it { should have_content("Email can't be blank") }
+  it { should have_content("Password is too short") }
+  it { should have_content("Password confirmation can't be blank") }
+ end
 end
 
 describe "with valid information" do
