a92eaca77498768a3c385d824d69e6ca19e7f49b
jbfink <john.fink@gmail.com>
2012-08-09 18:09:06 -0400

    Listing 8.5, the tests for signin failure.

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -7,4 +7,9 @@ describe "Authentication" do
    it { should have_selector('h1',	text: 'Sign in') }
    it { should have_selector('title',	text: 'Sign in') }
   end
+   describe "with invalid information" do
+   before { click_button "Sign in" }
+   it { should have_selector('title',   text: 'Sign in') }
+   it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+  end
 end
