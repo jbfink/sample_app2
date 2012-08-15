c6874f94ca48e8e85d20b3570283d89710407ea8
jbfink <john.fink@gmail.com>
2012-08-15 15:05:41 -0400

    Listing 8.10, an (unsuccessful) attempt at handling failed signin.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,9 +4,14 @@ class SessionsController < ApplicationController
   end
 
   def create
+     user = User.find_by_email(params[:session][:email].downcase)
+     if user && user.authenticate(params[:session][:password])
+       # Sign the user in and redirect to user's show page
+     else
+       flash[:error] = 'Invalid email/password combination' # Not quite right!
      render 'new'
   end
-
+ end
   def destroy
   end
 
