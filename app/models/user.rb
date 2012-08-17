03b21672d57a51106e2ec5d3dfa099b609963f84
jbfink <john.fink@gmail.com>
2012-08-17 15:49:52 -0400

    Oops. Forgot before_save :create_remember_token in app/models/user.rb.

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,7 @@ class User < ActiveRecord::Base
   attr_accessible :email, :name, :password, :password_confirmation
   has_secure_password
   before_save { self.email.downcase! }
+  before_save :create_remember_token
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
