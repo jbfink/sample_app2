adee301700136a6a191dc944a5ba806d04f1f079
jbfink <john.fink@gmail.com>
2012-08-17 16:07:54 -0400

    Listing 8.23, the signed_in? helper method.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,6 +3,9 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
     self.current_user = user
   end
+  def signed_in?
+    !current_user.nil?
+  end
   def current_user=(user)
     @current_user = user
   end
