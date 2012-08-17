4e6f4ae8a366fd203ddc260d660f60deb66e983e
jbfink <john.fink@gmail.com>
2012-08-17 16:01:52 -0400

    Listing 8.20, defining assignment to current_user.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,4 +3,7 @@ module SessionsHelper
     cookies.permanent[:remember_token] = user.remember_token
     self.current_user = user
   end
+  def current_user=(user)
+    @current_user = user
+  end
 end
